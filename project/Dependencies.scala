98199a840fe630524a9a7d8eba07d22ca441347a
Scala steward <me@scala-steward.org>
2019-01-14 20:24:21 +0100

    Update scala-logging to 3.9.2
    Closes #20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val commonmarkExtGfmTables = "com.atlassian.commonmark" % "commonmark-ext-gfm-tables" % commonmarkVersion
 
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
-  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
+  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
 
   val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2019.1"
 
