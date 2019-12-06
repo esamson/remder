e6235c018eaca709ece1428567df6e5b03456982
Scala Steward <me@scala-steward.org>
2019-12-06 16:26:47 +0100

    Update scalajs-dom to 1.0.0
    Closes #71

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,7 +46,7 @@ object Dependencies {
 
   val probeDeps = Def.setting(
     Seq[ModuleID](
-      "org.scala-js" %%% "scalajs-dom" % "0.9.7"
+      "org.scala-js" %%% "scalajs-dom" % "1.0.0"
     )
   )
 
