0936b15fcaf0b28aef4ca8a23c0edc5c2edd0858
Scala Steward <me@scala-steward.org>
2020-07-16 12:21:34 +0200

    Update akka-actor, akka-slf4j to 2.6.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 
 object Dependencies {
 
-  val akkaVersion = "2.6.7"
+  val akkaVersion = "2.6.8"
   val betterFilesVersion = "3.9.1"
 
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
