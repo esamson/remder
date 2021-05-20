4d592af67be92258800447133209101300e13803
Scala Steward <me@scala-steward.org>
2021-05-20 22:16:16 +0200

    Update javafx-web to 17-ea+11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
 
   val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2021.6"
 
-  val javafxWeb = "org.openjfx" % "javafx-web" % "16"
+  val javafxWeb = "org.openjfx" % "javafx-web" % "17-ea+11"
   val scalaFx = "org.scalafx" %% "scalafx" % "16.0.0-R24"
 
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9"
