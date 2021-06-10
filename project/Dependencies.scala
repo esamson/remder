7509868b783d8515ad36423b21a50a414e2dd652
Scala Steward <me@scala-steward.org>
2021-06-10 16:19:43 +0200

    Update akka-actor, akka-slf4j to 2.6.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 
 object Dependencies {
 
-  val akkaVersion = "2.6.14"
+  val akkaVersion = "2.6.15"
   val betterFilesVersion = "3.9.1"
 
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
