b58b4b119d62d7e5d148fe53775a318649a22529
Scala Steward <me@scala-steward.org>
2021-09-17 23:26:18 +0200

    Update scalatest to 3.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
   val javafxWeb = "org.openjfx" % "javafx-web" % "16"
   val scalaFx = "org.scalafx" %% "scalafx" % "16.0.0-R24"
 
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10"
 
   val appDeps = Def.setting(
     Seq(
