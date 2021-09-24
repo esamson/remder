42569b182e6a7331e19610f862fc4a75d80f3b83
Scala Steward <me@scala-steward.org>
2021-09-24 16:16:14 +0200

    Update javafx-web to 18-ea+3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
 
   val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2021.10"
 
-  val javafxWeb = "org.openjfx" % "javafx-web" % "16"
+  val javafxWeb = "org.openjfx" % "javafx-web" % "18-ea+3"
   val scalaFx = "org.scalafx" %% "scalafx" % "16.0.0-R25"
 
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10"
