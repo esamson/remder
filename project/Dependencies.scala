b07879c62ccef2602f1b49473ab55e08bdbeb61c
Scala Steward <scala_steward@virtuslab.com>
2022-12-14 15:34:54 +0000

    Update javafx-web to 20-ea+11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
 
   val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2022.14"
 
-  val javafxWeb = "org.openjfx" % "javafx-web" % "20-ea+9"
+  val javafxWeb = "org.openjfx" % "javafx-web" % "20-ea+11"
 
   val scalaFx = "org.scalafx" %% "scalafx" % "19.0.0-R30"
 
