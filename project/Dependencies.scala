c9376fa721c5cad02e8e4f5070a31089911cff4a
Scala Steward <scala_steward@virtuslab.com>
2026-01-28 21:37:03 +0000

    Update plantuml to 1.2026.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.5.25"
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.6"
 
-  val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2026.0"
+  val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2026.1"
 
   val javafxWeb = "org.openjfx" % "javafx-web" % "23.0.2"
 
