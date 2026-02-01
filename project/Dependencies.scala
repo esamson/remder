b76880751e94cb1a28341bfeb4f08b33d7038277
Scala Steward <scala_steward@virtuslab.com>
2026-02-01 12:57:17 +0000

    Update logback-classic to 1.5.27

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val commonmarkExtGfmTables =
     "org.commonmark" % "commonmark-ext-gfm-tables" % commonmarkVersion
 
-  val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.5.25"
+  val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.5.27"
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.6"
 
   val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2026.1"
